0fe94d29e3d16d232003757ae82e19635ab5072d
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-09 13:26:16 -0600

    feat: :construction: Create button component

---
+++ src/components/button.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+export default function Button({ type, color, text }) {
+  const colorVariants = {
+    blue: "bg-blue-600 hover:bg-blue-500 text-white",
+    green: "bg-green-500 hover:bg-green-400 text-white",
+    red: "bg-red-500 hover:bg-red-400 text-white",
+    yellow: "bg-yellow-300 hover:bg-yellow-400 text-black",
+  };
+
+  return (
+    <button
+      type={type}
+      className={`${colorVariants[color]} rounded px-4 py-2 shadow-sm`}
+    >
+      {text}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  color: "green",
+  type: "button",
+};
+
+Button.propTypes = {
+  type: PropTypes.string,
+  color: PropTypes.string,
+  text: PropTypes.string.isRequired,
+};

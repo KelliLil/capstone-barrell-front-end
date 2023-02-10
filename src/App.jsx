28bb0dc233d64b3f08037381a0ae145e97826c67
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-10 08:51:42 -0600

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginRegister from "./routes/login-register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginRegister />,
+  },
+]);
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
